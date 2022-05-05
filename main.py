ebcf7bb768ae81211d505816694b70ddcf920c1a
Ronaldy <ronaldylf@gmail.com>
2022-05-05 00:29:03 -0300

    apresentation version finale

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 try:
     # variaveis, funcoes e setup dos componentes
-    exec(open('./main_functions.py').read(),globals())
-    # toca uma nota musical para indicar sucesso no arranque inicial
+    exec(open('./main_functions.py').read(), globals())
+    # toca uma nota musical bem rápido para indicar sucesso no arranque inicial
     from machine import PWM, Pin
-    import time, os
+    import time
+    import os
     pwm25 = PWM(Pin(25), freq=(1),  duty=512)
     time.sleep(0.1)
     pwm25.deinit()
     os.chdir('/')
 except Exception as e:
-    # toca uma nota musical para indicar erro em alguma parte do programa de configuracao e encerra
+    # toca uma nota musical por mais tempopara indicar erro em alguma parte do programa de configuracao e encerra
     print("ERROR: " + str(e))
     import sys
     import time
@@ -32,24 +33,23 @@ while (True):
         delta_seconds = t - t0
         percentage_complete = round((delta_seconds/(max_delta*60))*100, 2)
         print(f"({round(delta_seconds/60, 2)} minutos) {delta_seconds}/{(max_delta*60)} ({max_delta} minutos) -> {percentage_complete}%")
-        
 
     print("\n")
     print("iniciando rotina")
-    cycle_current += 1  # adiciona 1 a quantidade de execues
+    cycle_current += 1  # adiciona 1 a quantidade de execucoes
     gps_data = GPS()
 
-    # objeto dos dados juntamente com o payload
     print("coletando dados dos sensores...")
+    # objeto dos dados juntamente com o payload
     general_data = getData({
         "umidade": Humidity(),
         "co2": CO2(),
-        "datetime": "%d/%d/%d %d:%d:%d"%gps_data['datetime']
+        "datetime": "%d/%d/%d %d:%d:%d" % gps_data['datetime']
     })
     print("dados coletados:")
-
     # printa os dados de cima pra baixo
-    for key in general_data: print((key, general_data[key]))
+    for key in general_data:
+        print((key, general_data[key]))
 
     # transforma os dados para uma string em formato JSON
     json_data = json.dumps(general_data)
@@ -61,19 +61,19 @@ while (True):
     data_file.close()
     print("dados armazenados")
 
-    # posiciona um cone no mapa com base nos dados do sensor gps
+    # posiciona um icone no mapa com base nos dados do sensor gps
     addMapPosition(
-        f"CubeSatPoincare({cycle_current})",
+        f"CubeSatPoincare({cycle_current})",  # título
         gps_data['latitude'],  # latitude
         gps_data['longitude'],  # longitude
-        session_id,  # id de sesso
-        str(gps_data),  # numero da execucao
-        url_icon  # url do cone
+        session_id,  # id de sessao
+        str(gps_data),  # dados do gps
+        url_icon  # url do icone
     )
     print("localização definida no mapa")
 
-    # envia a requisio HTTP pelo metodo POST para a sonda Zenith e para outros endereços
-    print("enviando para servidores remotos...")
+    # envia a requisicao HTTP pelo metodo POST para a sonda Zenith e para outros endereços
+    print(f"enviando para {len(payload_addresses)} servidores remotos...")
     for address_current in payload_addresses:
         response = urequests.post(url=address_current, json=json_data)
         print(f"enviado para {address_current}")
--- main_functions.py
+++ main_functions.py
@@ -1,4 +1,5 @@
-# importao dos mdulos para processamento e manipulao dos dados
+# importao dos mdulos para processamento e manipulacao dos dados
+import sdcard
 from machine import (
     Pin,
     SoftI2C,
@@ -7,7 +8,6 @@ from machine import (
     PWM,
     SPI
 )
-
 import network
 import urequests
 from mini_micropyGPS import MicropyGPS
@@ -16,59 +16,60 @@ from mpu9250 import MPU9250
 import CCS811
 import os
 import json
-import utime, time
+import utime
+import time
 import sys
 import _thread
 
 # variveis editveis
 team_id = 41  # id da equipe
-session_id = 1111  # id de sessoes
+session_id = 1111  # id de sessao para visualizacao do satelite no mapa
 wifi_ssid = 'OBSAT_WIFI'  # nome da rede wifi
 wifi_password = 'OBSatZenith1000'  # senha da rede wifi
-# endereo na qual sero enviadas as requisies com os dados
+# endereco na qual serao enviadas as requisicoes com os dados
 payload_addresses = [
-    #"http://192.168.0.1/",
-    "http://ptsv2.com/t/5gwcr-1651146427/post",
-    #"http://localhost:80/sendData",
+    "http://192.168.0.1/",  # sonda
+    "http://ptsv2.com/t/5gwcr-1651146427/post",  # endereço terrestre
 ]
-url_icon = "https://i.imgur.com/nHMQQ2Y.jpeg",  # url do cone
-max_delta = 4 # quantos minutos para cada execucao (a cada 4 minutos uma execucao)
+# url do icone que aparecera no mapa
+url_icon = "https://i.imgur.com/nHMQQ2Y.jpeg"
+# quantos minutos para cada execucao (a cada 4 minutos uma execucao)
+max_delta = 4
 
 # configuracao gps
 uartGPS = UART(2, tx=17, rx=13)
 uartGPS.init(9600, bits=8, parity=None, stop=1)
-gps = MicropyGPS(-3, "dd") # utc-3 (fuso horário maranhense)
+gps = MicropyGPS(-3, "dd")  # utc-3 (fuso horário maranhense)
 gpsdata = {}
 time.sleep(1)
+
+
 def updateGPS():
-    # funcao para ficar constantemente atualizando os dados do gps
-    global gpsdata
-    global uartGPS
-    global gps
+    # fica constantemente atualizando os dados do gps
     while True:
+        global gpsdata
+        global uartGPS
+        global gps
         if uartGPS.any():
             c = int.from_bytes(uartGPS.read(1), "big")
             stat = gps.update(chr(c))
         else:
             gpsdata['error'] = "gps not found"
 
+
+# funciona de forma paralela, para que nao interrompa o fluxo principal do programa
 _thread.start_new_thread(updateGPS, ())
 print("gps OK")
 
 
 # configuracao microSD
 print("preparando cartao sd")
-import sdcard
 sd = sdcard.SDCard(SPI(2), Pin(15))
 try:
     os.mount(sd, '/sd')
 except:
     pass
 print("SDCard OK")
-
-# variaveis de teste (apagar após os testes)
-wifi_ssid = 'kaiju'  # nome da rede wifi
-wifi_password = '1234567890'  # senha da rede wifi
 
 # conexao com a rede wifi
 print("preparando wifi")
@@ -78,11 +79,13 @@ if not sta_if.isconnected():
     print('conectando')
     sta_if.active(True)
     sta_if.connect(wifi_ssid, wifi_password)
-    while not sta_if.isconnected(): pass
-
+    while not sta_if.isconnected():
+        pass
 print("wifi OK")
 
+
 def clearSDCard():
+    # limpar dados do cartão SD caso necessario
     files = os.listdir("/sd")
     for file in files:
         try:
@@ -91,9 +94,10 @@ def clearSDCard():
             # arquivos obrigatórios
             pass
 
+
 def GPS():
-    # funcao que retorna os dados do gps
-    # latitude e longitude em graus decimais 
+    # retorna os dados do gps
+    # latitude e longitude em graus decimais
     # altitude em metros
     # velocidade em km/h
     # datetime em (dia, mes, ano, hora, minuto, segundo)
@@ -104,13 +108,18 @@ def GPS():
         "velocidade": gps.speed[2],
         "datetime": tuple(list(gps.date) + list(gps.timestamp))
     }
-    if gpsdata['latitude'][1]=='S': gpsdata['latitude'] = -gpsdata['latitude'][0]
-    if gpsdata['longitude'][1]=='W': gpsdata['longitude'] = -gpsdata['longitude'][0]
-    if 'error' in gpsdata: del gpsdata['error']
+    if gpsdata['latitude'][1] == 'S':
+        gpsdata['latitude'] = -gpsdata['latitude'][0]
+    if gpsdata['longitude'][1] == 'W':
+        gpsdata['longitude'] = -gpsdata['longitude'][0]
+    if 'error' in gpsdata:
+        del gpsdata['error']
     return gpsdata
 
+
 def addMapPosition(name, latitude, longitude, session, info, icon):
-    params={
+    # adiciona a localizacao do satelite no mapa com base nos dados retornados pelo gps
+    params = {
         'name': name,
         'lat': latitude,
         'long': longitude,
@@ -126,26 +135,11 @@ def addMapPosition(name, latitude, longitude, session, info, icon):
         params_str += f"{key}={value}&"
 
     params_str = params_str[:-1]
-    print(url+params_str)
     response = urequests.get(url+params_str)
 
 
-# def takePhoto(filename=None):
-#     '''
-#     funo retorna uma foto e armazena no
-#     carto SD caso seja passado o nome do arquivo
-#     no parmetro 'filename'
-#     '''
-#     photo = camera.capture()  # tira a foto
-#     if (type(filename) == str):
-#         file = open("fotos/"+filename+".jpg", "bw")
-#         file.write(photo)
-#         file.close()
-#     return photo
-
-
 def batteryLevel():
-    # funo que retorna o nvel da bateria em porcentagem
+    # retorna o nivel da bateria em porcentagem
     adc35 = ADC(Pin(35))
     adc35.atten(ADC.ATTN_11DB)
     adc35.width(ADC.WIDTH_12BIT)
@@ -154,7 +148,7 @@ def batteryLevel():
 
 
 def Temperature():
-    # funo que retorna a atual temperatura
+    # retorna a temperatura em graus celsius
     i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
     i2c.writeto(0x40, b'\xf3')
     time.sleep_ms(70)
@@ -164,7 +158,7 @@ def Temperature():
 
 
 def Pressure():
-    # funo que retorna a atual presso
+    # retorna a pressao em Pascal
     bus = SoftI2C(scl=Pin(22), sda=Pin(21))
     bmp280 = BMP280(bus)
     bmp280.use_case(BMP280_CASE_WEATHER)
@@ -175,7 +169,7 @@ def Pressure():
 
 
 def Gyro():
-    # funo que retorna a atual taxa de giro nos eixos X, Y e Z
+    # retorna a taxa de giro nos eixos X, Y e Z
     i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
     mpu9250s = MPU9250(i2c)
     gyro_xyz = tuple(mpu9250s.gyro)
@@ -184,49 +178,53 @@ def Gyro():
 
 
 def Acceleration():
-    # funo que retorna a atual taxa de acelerao nos eixos X, Y e Z
+    # retorna a  taxa de aceleracao nos eixos X, Y e Z
     i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
     mpu9250s = MPU9250(i2c)
     acceleration_xyz = tuple(mpu9250s.acceleration)
-    
+
     return acceleration_xyz
 
+
 def CO2():
-    # nível de CO2
+    # retorna o nível de CO2 em partes por milhao (ppm)
     bus = SoftI2C(scl=Pin(22), sda=Pin(21))
     sCCS811 = CCS811.CCS811(i2c=bus, addr=0x5A)
     ok = sCCS811.data_ready()
     return sCCS811.eCO2
 
+
 def Humidity():
-    # umidade
+    # retorna a umidade em porcentagem
     i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
-    i2c.writeto(0x40,b'\xf5')
+    i2c.writeto(0x40, b'\xf5')
     time.sleep_ms(70)
     t = i2c.readfrom(0x40, 2)
     return -6+125*(t[0]*256+t[1])/65535
 
+
 def Luminosity():
-    # luminosidade
+    # retorna a luminosidade em porcentagem
     adc34 = ADC(Pin(34))
     adc34.atten(ADC.ATTN_11DB)
     adc34.width(ADC.WIDTH_12BIT)
-    light = adc34.read() #max: 4095
+    light = adc34.read()  # max: 4095
     return round(light, 2)
 
 
 def Magnetic():
-    # campo magnetico
+    # retorna o campo magnetico em micro Tesla
     i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
     mpu9250s = MPU9250(i2c)
     magnetism_xyz = tuple(mpu9250s.magnetic)
 
     return magnetism_xyz
 
+
 def getData(payload={}):
     ''' 
-    funo que retorna os dados dos sensores juntamente com
-    o payload passado no parmetro da funo.
+    retorna os dados dos sensores juntamente com
+    o payload passado no parametro da funcao
     '''
     data = {
         "equipe": team_id,  # id da equipe
@@ -239,6 +237,8 @@ def getData(payload={}):
     }
     return data
 
+
 def debug():
     # apenas para na linha da funcao para fins de testes
-    while(True): time.sleep(5)
+    while(True):
+        time.sleep(5)
